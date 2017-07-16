c690512cbeebc4fd7ad43fa3cd01be8443c37019
Jinmo Chong <jinmo55555@gmail.com>
2017-07-16 16:40:31 -0400

    make view headline

--- music/views.py
+++ music/views.py
@@ -4,7 +4,7 @@ from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums':all_albums}
+    context = {'all_albums': all_albums}
     return render(request, 'music/index.html', context)
 
 
