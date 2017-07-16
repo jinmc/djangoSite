2a5bcf32b2ffb912e491271b2157751e7a080caf
Jinmo Chong <jinmo55555@gmail.com>
2017-07-16 13:13:00 -0400

    connect to database

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+
     url(r'^$', views.index, name='index'),
+
+    # /music/71/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
 ]
