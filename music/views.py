b53d905b4070dbec2abcf3ae12d5e3c5ac9417f7
Jinmo Chong <jinmo55555@gmail.com>
2017-07-16 17:40:23 -0400

    add gitignore

--- music/views.py
+++ music/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Album
+from django.http import Http404
+
+
 
 def index(request):
     all_albums = Album.objects.all()
@@ -9,4 +11,8 @@ def index(request):
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/index.html', {'all_albums' : album})
