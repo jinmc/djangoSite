118129c64ed874aa5d1411262dcc7bbbf82664aa
Jinmo Chong <jinmo55555@gmail.com>
2017-07-16 17:56:32 -0400

    raise 404 error

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Album
 from django.http import Http404
+from django.http import HttpResponse
 
 
 
@@ -11,8 +12,9 @@ def index(request):
 
 
 def detail(request, album_id):
+    # return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
     try:
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
         raise Http404("Album does not exist")
-    return render(request, 'music/index.html', {'all_albums' : album})
+    return render(request, 'music/detail.html', {'album' : album})
