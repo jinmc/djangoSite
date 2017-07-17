a7740235e429e84f5c1d26d651c539a29b1a1f88
Jinmo Chong <jinmo55555@gmail.com>
2017-07-17 08:16:36 -0400

    add .idea to gitignore

--- music/views.py
+++ music/views.py
@@ -13,9 +13,10 @@ def index(request):
 
 def detail(request, album_id):
     # return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+
     # try:
     #     album = Album.objects.get(pk=album_id)
     # except Album.DoesNotExist:
     #     raise Http404("Album does not exist")
-    album = get_object_or_404(Album, pk=album_id)
+    album = get_object_or_404(Album, pk=album_id) # same as above code
     return render(request, 'music/detail.html', {'album' : album})
